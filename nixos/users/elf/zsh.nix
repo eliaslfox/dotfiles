0925954834a7e02d386f63dbc48e96a3f3f7409b
Elias Lawson-Fox <me@eliaslfox.com>
2019-05-23 16:41:24 -0700

    fixed dotfiles

--- nixos/users/elf/zsh.nix
+++ nixos/users/elf/zsh.nix
@@ -14,10 +14,10 @@
     XDG_DATA_HOME="$HOME/.local/share";
 
     /* Use XDG */
-    GNUPGHOME="$XDG_CONFIG_HOME/gnupg";
-    NIXOPS_STATE="$XDG_CONFIG_HOME/nixops/deployments.nixops";
-    INPUTRC="$XDG_CONFIG_HOME/readline/inputrc";
-    NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc";
+    GNUPGHOME="$HOME/.config/gnupg";
+    NIXOPS_STATE="$HOME/.config/nixops/deployments.nixops";
+    INPUTRC="$HOME/.config/readline/inputrc";
+    NPM_CONFIG_USERCONFIG="$HOME/.config/npm/npmrc";
 
     /* Handle temp ~ */
     PASSWORD_STORE_DIR = "$HOME/Documents/password-store";
