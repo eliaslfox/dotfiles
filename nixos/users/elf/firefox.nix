484b543bca3305345033ee6e7caf3306cd452d74
Elias Lawson-Fox <me@eliaslfox.com>
2020-06-16 08:26:11 -0700

    fix firefox url bar

--- nixos/users/elf/firefox.nix
+++ nixos/users/elf/firefox.nix
@@ -11,6 +11,46 @@ let
     #sidebar-header {
         visibility: collapse;
     }
+
+    /*** BEGIN Firefox 77 (June 2, 2020) Override URL bar enlargement ***/
+    /***  https://support.mozilla.org/en-US/questions/1290682         ***/
+
+    /* Compute new position, width, and padding */
+
+    #urlbar[breakout][breakout-extend] {
+      top: 5px !important;
+      left: 0px !important;
+      width: 100% !important;
+      padding: 0px !important;
+    }
+    /* for alternate Density settings */
+    [uidensity="compact"] #urlbar[breakout][breakout-extend] {
+      top: 3px !important;
+    }
+    [uidensity="touch"] #urlbar[breakout][breakout-extend] {
+      top: 4px !important;
+    }
+
+    /* Prevent shift of URL bar contents */
+
+    #urlbar[breakout][breakout-extend] > #urlbar-input-container {
+      height: var(--urlbar-height) !important;
+      padding: 0 !important;
+    }
+
+    /* Do not animate */
+
+    #urlbar[breakout][breakout-extend] > #urlbar-background {
+      animation: none !important;;
+    }
+
+    /* Remove shadows */
+
+    #urlbar[breakout][breakout-extend] > #urlbar-background {
+      box-shadow: none !important;
+    }
+
+    /*** END Firefox 77 (June 2, 2020) Override URL bar enlargement ***/
   '';
 
   userChromeClean = ''
@@ -214,7 +254,8 @@ let
 
   };
 
-in {
+in
+{
   enable = true;
   profiles = {
     default = { inherit userChrome settings; };
